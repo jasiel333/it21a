5c9441839f6007f224e57bb01a4d5b10ebfe7ac6
jasiel333 <jasieldumana951@gmail.com>
2025-09-15 08:35:07 +0800

    Added Mage fireball

--- hero.js
+++ hero.js
@@ -17,8 +17,24 @@ class Warrior extends Hero{
     console.log('${this.name} uses power Strike');
   }
 }
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+
+useAbility(){
+    console.log('${this.name} uses fireball');
+}
+
+}
+
+
 
 const warrior = new Warrior("Thorin",100,10);
 warrior.useAbility()
 
 const hero = new Hero("Thorin",100,10);
+
+const mage = new Mage("Gandalf",80,8,50);
+mage.useAbility();
