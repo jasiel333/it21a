0f28ef71d399829f311da7535a6f51117e4ce8d9
jasiel333 <jasieldumana951@gmail.com>
2025-09-15 08:24:38 +0800

    Added Warrior subclas and update instance

--- hero.js
+++ hero.js
@@ -12,7 +12,13 @@ class Hero{
      }
 }
 
-const hero = new Hero("Thorin",100,10);
+class Warrior extends Hero{
+  useAbility(){
+    console.log('${this.name} uses power Strike');
+  }
+}
 
-console.log(hero.getHealth());
-hero.getHealth();
+const warrior = new Warrior("Thorin",100,10);
+warrior.useAbility()
+
+const hero = new Hero("Thorin",100,10);
